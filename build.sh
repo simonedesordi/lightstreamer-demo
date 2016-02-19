2add283b5ec6633b07b8cc342545932379bbadf4
simonedesordi <simone.desordi@gmail.com>
2016-02-19 09:08:51 +0100

    intial version

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 javac -classpath lib/ls-adapter-interface.jar -d tmp_classes -sourcepath src src/HelloWorldDataAdapter.java
 jar cvf HelloWorldDataAdapter.jar -C tmp_classes .
+cp adapter.xml ~/dev/workspace/lightstreamer/server/adapters/demo/
+cp HelloWorldDataAdapter.jar ~/dev/workspace/lightstreamer/server/adapters/demo/lib/
