27050a47277056445f9cd20d8a28b3cc0eac4262
simonedesordi <simone.desordi@gmail.com>
2016-02-19 15:36:43 +0100

    first step, first iteration

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 javac -classpath lib/ls-adapter-interface.jar -d tmp_classes -sourcepath src src/HelloWorldDataAdapter.java
 jar cvf HelloWorldDataAdapter.jar -C tmp_classes .
-cp adapter.xml ~/dev/workspace/lightstreamer/server/adapters/demo/
+cp adapters.xml ~/dev/workspace/lightstreamer/server/adapters/demo/
+cp adapters_log_conf.xml ~/dev/workspace/lightstreamer/server/adapters/demo/
 cp HelloWorldDataAdapter.jar ~/dev/workspace/lightstreamer/server/adapters/demo/lib/
